4a430f60354b4b51efd0fba9417ab8fcba0e923d
rainautumn <rain@now.zz.mu>
2016-04-29 19:07:53 +0300

    camera animation

--- src/camera.cpp
+++ src/camera.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-#define sence 4000.0
+#define sence 1000.0
 using namespace std;
 
 
@@ -16,18 +16,43 @@ Camera::Camera()
 {
     _res_pos.position.drop();
     _res_pos.angular_position.drop();
-    _res_pos.position.init(0, 0, -10);
+    _res_pos.position.init(0, -2, -10);
+}
+
+void Camera::set_perspective(bool value)
+{
+	perspective = value;
+}
+
+void Camera::set_default_position(bool value)
+{
+	default_position = value;
 }
 
 mtx4 Camera::get_matrix()
 {
     mtx4 buf_position;
-    buf_position.tranform_position(_res_pos.position);
-    mtx4 buf_angular;
-    buf_angular.tranform_angle(_res_pos.angular_position);
+	mtx4 buf_angular;
+	if(default_position)
+	{
+		buf_position.init();
+		buf_angular.init();
+	}
+	else
+	{
+		_res_pos.acceleration -= _res_pos.acceleration / 10;
+		_res_pos.velocity -= _res_pos.velocity / 10;
+	    _res_pos.velocity += _res_pos.acceleration / 4;
+	    _res_pos.position += _res_pos.velocity / 4;
+		buf_position.tranform_position(_res_pos.position);
+		buf_angular.tranform_angle(_res_pos.angular_position);
+	}
     mtx4 buf_perspective;
-    buf_perspective.perspective(1,1,0.5,150);
-    get_res_cam(buf_angular);
+    if(perspective)
+    	buf_perspective.perspective(1,1,1,150);
+    else
+    	buf_perspective.init();
+    set_res_cam(buf_angular);
 
     return buf_perspective * buf_angular * buf_position;
 }
@@ -41,17 +66,17 @@ void Camera::set_mouse(double xpos, double ypos)
 void Camera::set_keymap(int key, int scancode, int action, int mods)
 {
     if(key == GLFW_KEY_W && action == GLFW_PRESS)
-        _res_pos.position += _res_cam.normal.orterisation();
+        _res_pos.acceleration = _res_cam.normal.orterisation();
     if(key == GLFW_KEY_S && action == GLFW_PRESS)
-        _res_pos.position -= _res_cam.normal.orterisation();
+        _res_pos.acceleration = - _res_cam.normal.orterisation();
 
     if(key == GLFW_KEY_A && action == GLFW_PRESS)
-        _res_pos.position += _res_cam.tangent.orterisation();
+        _res_pos.acceleration = _res_cam.tangent.orterisation();
     if(key == GLFW_KEY_D && action == GLFW_PRESS)
-        _res_pos.position -= _res_cam.tangent.orterisation();
+        _res_pos.acceleration = - _res_cam.tangent.orterisation();
 }
 
-void Camera::get_res_cam(mtx4 buf_mtx)
+void Camera::set_res_cam(mtx4 buf_mtx)
 {
     _res_cam.tangent.init(buf_mtx.m[0][0],
                           buf_mtx.m[0][1],

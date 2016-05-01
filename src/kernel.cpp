318e57b7272a34f5be49def45049a46367ba41d9
aleksei-udalov <aleksei.udalov.01@gmail.com>
2016-05-02 00:36:11 +0300

    kernel get screen resolution from command line

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -9,17 +9,23 @@
 Kernel::Kernel(int argc, char** argv)
 {
 	/* parse command line arguments */
-	static const char *optString = "m:";
+	static const char *optString = "m:h:w:";
 	int opt = 0;
 	opt = getopt(argc, argv, optString);
 	while(opt != -1) {
 		switch(opt) {
 			case 'm':
-				printf("%s\n", optarg);
 				map_name = optarg;
+				break;
+			case 'h':
+				height = atoi(optarg);
+				break;
+			case 'w':
+				width = atoi(optarg);
 				break;
 		}
 		opt = getopt(argc, argv, optString);
 	}
+	printf("Resolution set to %fx%f\n", width, height);
 	printf("Loading map %s\n", map_name);
 };
--- src/kernel.h
+++ src/kernel.h
@@ -1,7 +1,12 @@
+#define DEF_HEIGHT 640.0
+#define DEF_WIDTH 800.0
+
 class Kernel
 {
 public:
 	Kernel(int argc, char **argv);
 private:
 	char* map_name;
+	float width;
+	float height;
 };

e3dd60de7d90bd0d5dc7ab806da55b1ad3220b44
Alex <alex.potapov21@gmail.com>
2016-05-09 01:33:34 +0300

    Added network

--- src/network.cpp
+++ src/network.cpp
@@ -12,6 +12,33 @@ void Network::init_camera(Camera *camera)
 
 void Network::packet()
 {
+
 //    qDebug()<<_camera->_res_pos.angular_position.v[1];
 
 }
+
+void Network::initSocket()
+  {
+      udpSocket = new QUdpSocket(this);
+      udpSocket->bind(QHostAddress::LocalHost, 7755);
+
+      connect(udpSocket, SIGNAL(readyRead()),
+              this, SLOT(readPendingDatagrams()));
+  }
+
+void Network::readPendingDatagrams()
+{
+    while (udpSocket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(udpSocket->pendingDatagramSize());
+        QHostAddress sender;
+        quint16 senderPort;
+
+        udpSocket->readDatagram(datagram.data(), datagram.size(),
+                                &sender, &senderPort);
+
+        //processTheDatagram(datagram);
+    }
+}
+
+
--- src/network.h
+++ src/network.h
@@ -2,15 +2,21 @@
 #define NETWORK_H
 
 #include <QObject>
+#include <QUdpSocket>
 #include "camera.h"
-class Network
+class Network:QObject
 {
+    Q_OBJECT
 public:
     Network();
     void init_camera(Camera * camera);
     void packet();
+    void initSocket();
 private:
     Camera * _camera;
+    QUdpSocket * udpSocket;
+private slots:
+    void readPendingDatagrams();
 };
 
 #endif // NETWORK_H
